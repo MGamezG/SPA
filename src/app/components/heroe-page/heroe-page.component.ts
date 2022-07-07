d676d1af95f3ace88eac6f2ebacface7e37b5dad
MGamezG <l171130314@gamadero.tecnm.mx>
2022-07-07 17:58:06 -0500

    pipes

--- src/app/components/heroe-page/heroe-page.component.ts
+++ src/app/components/heroe-page/heroe-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { HeroesService } from 'src/app/services/heroes.service';
 
 @Component({
   selector: 'app-heroe-page',
@@ -6,8 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroe-page.component.scss']
 })
 export class HeroePageComponent implements OnInit {
-
-  constructor() { }
+  heroe:any={};
+  constructor(private actiateRouter:ActivatedRoute,
+              private heroService:HeroesService) {
+                this.actiateRouter.params.subscribe(
+                  (params:Params)=>{
+                    console.log(params['id']);
+                    this.heroe=this.heroService.getHeroe(params['id']);
+                  }
+                )
+              }
 
   ngOnInit(): void {
   }
