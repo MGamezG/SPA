44e84522d0bc9e3284be2569d74489acc347fb7f
MGamezG <l171130314@gamadero.tecnm.mx>
2022-07-06 19:39:09 -0500

    rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'home',
+    component:HomeComponent
+  },
+  {
+    path:'',
+    redirectTo:'home',
+    pathMatch:'full'
+    //redirecciona al primer componente, o al home
+    //dependiendo de la ruta espesificada para la redireccion
+  },
+  {
+    path:'heroes',
+    component:HeroesComponent
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
+//modulos
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+//componentes
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+//rutas
+import { AppRoutingModule } from './app-routing.module';
+//servicios
 
 @NgModule({
   declarations: [
