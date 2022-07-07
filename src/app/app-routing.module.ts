2e263f2f37f3cc8f474312d543103fb73bdcea65
MGamezG <l171130314@gamadero.tecnm.mx>
2022-07-06 19:58:44 -0500

    routerLink y routerLinkActive

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { HomeComponent } from './components/home/home.component';
 
@@ -19,6 +20,10 @@ const routes: Routes = [
     path:'heroes',
     component:HeroesComponent
   },
+  {
+    path:'about',
+    component:AboutComponent
+  }
 
 ];
 
